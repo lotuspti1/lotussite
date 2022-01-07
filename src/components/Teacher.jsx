fcdd9d86a86b8c55f15e86ea4a804b0b4e8cd46c
amrit <all.categories2222@gmail.com>
2022-01-07 16:12:06 +0545

    Teacher image

--- src/components/Teacher.jsx
+++ src/components/Teacher.jsx
@@ -5,7 +5,7 @@ return(
         <div className='container-fluid'>
             <div className='row teacherRow'>
                 <div className='col-sm-4'>
-                    <img className='img-responsive timage' src="https://english.nepalpress.com/wp-content/uploads/2021/01/school.jpg" alt='First Slide'></img>
+                    <img className='img-responsive timage' src="https://image.shutterstock.com/image-vector/male-teacher-standing-front-blackboard-260nw-402654784.jpg" alt='First Slide'></img>
                 </div>
                 <div className='col-sm-8 teacherDetails'>
                     <text id='tname'>Prakash Chalise</text>
@@ -17,7 +17,7 @@ return(
             </div>
             <div className='row teacherRow'>
                 <div className='col-sm-4'>
-                    <img className='img-responsive timage' src="https://english.nepalpress.com/wp-content/uploads/2021/01/school.jpg" alt='First Slide'></img>
+                    <img className='img-responsive timage' src="https://image.shutterstock.com/image-vector/male-teacher-standing-front-blackboard-260nw-402654784.jpg" alt='First Slide'></img>
                 </div>
                 <div className='col-sm-8 teacherDetails'>
                     <text id='tname'>Prakash Chalise</text>
@@ -29,7 +29,7 @@ return(
             </div>
             <div className='row teacherRow'>
                 <div className='col-sm-4'>
-                    <img className='img-responsive timage' src="https://english.nepalpress.com/wp-content/uploads/2021/01/school.jpg" alt='Teacher Image'></img>
+                    <img className='img-responsive timage' src="https://image.shutterstock.com/image-vector/male-teacher-standing-front-blackboard-260nw-402654784.jpg" alt='Teacher Image'></img>
                 </div>
                 <div className='col-sm-8 teacherDetails'>
                     <text id='tname'>Prakash Chalise</text>
